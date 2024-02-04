2956e8f2c03040cb244edf7b9b7a5dddbd2b8219
Renn <119943753+REnnnnxde@users.noreply.github.com>
2024-02-04 09:36:08 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
+// Import modul React dan ReactDOM yang diperlukan
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// Komponen fungsional Hello untuk merender pesan "Hello World!"
+function Hello(props) {
+  return <h1>Hello World!</h1>;
+}
+
+// Komponen App menggunakan arrow function untuk merender pesan "Hello ini APP !"
+const App = () => {
+  return <div> Hello ini APP !</div>;
+}
+
+// Komponen MainApp yang menggabungkan komponen App dan Hello dalam satu div
+const MainApp = () => (
+  <div>
     <App />
-  </React.StrictMode>
+    <Hello />
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Mengambil elemen HTML dengan ID "root" sebagai tempat mounting React
+const container = document.getElementById("root");
+
+// Membuat instance root dengan createRoot menggunakan container sebagai target mounting
+const root = ReactDOM.createRoot(container);
+
+// Melakukan render dengan komponen MainApp, yang merender App dan Hello bersamaan
+root.render(<MainApp />);
